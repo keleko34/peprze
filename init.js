d57a266721a97b8a0dbcd51ecb66288b7cd39f33
= <gregory.guidero@gmail.com>
2019-01-30 12:44:30 +0100

    all values passed from previous promises when new promises are returned from then statements

--- init.js
+++ init.js
@@ -2,14 +2,13 @@ window.Promise = (function(){
   
   function Promise(res, resolvers, rejecters)
   {
-    this.__value = void 0;
+    this.__value = [void 0];
     this.__resolve = (resolvers ? resolvers : []);
     this.__reject = (rejecters ? rejecters : []);
     this.__fulfilled = false;
     this.__rejected = false;
     this.__finished = false;
     this.__base = undefined;
-    this.__passedValue = undefined;
     
     this.resolve = (function()
     {
@@ -17,21 +16,12 @@ window.Promise = (function(){
           __resolver = this.__resolve.splice(0,1)[0];
 
       this.__fulfilled = true;
-      if(arguments[0]) this.__value = arguments[0];
+      if(arguments[0]) this.__value[0] = arguments[0];
 
       if(__resolver)
       {
         try{
-
-          if(this.base)
-          {
-            __value = __resolver.call(this.base, this.__value, this.__passedValue);
-          }
-          else
-          {
-            __value = __resolver(this.__value, this.__passedValue);
-          }
-
+          __value = __resolver.apply((this.base || this), this.__value);
         }
         catch(e)
         {
@@ -43,17 +33,10 @@ window.Promise = (function(){
         {
           __value.__resolve = __value.__resolve.concat(this.__resolve);
           __value.__reject = __value.__reject.concat(this.__reject);
-          __value.__passedValue = this.__value;
+          __value.__value = __value.__value.concat(this.__value);
           if(__value.__fulfilled || __value.__finished)
           {
-            if(__value.base)
-            {
-              __value.resolve.call(__value.base, __value.__value, __value.__passedValue);
-            }
-            else
-            {
-              __value.resolve(__value.__value, __value.__passedValue);
-            }
+            __value.resolve.apply((__value.base || __value), __value.__value);
           }
         }
         else
@@ -74,21 +57,12 @@ window.Promise = (function(){
 
       this.__fulfilled = true;
       this.__rejected = true;
-      if(arguments[0]) this.__value = arguments[0];
+      if(arguments[0]) this.__value[0] = arguments[0];
 
       if(__rejecter)
       {
         try{
-
-          if(this.base)
-          {
-            __value = __rejecter.call(this.base, this.__value);
-          }
-          else
-          {
-            __value = __rejecter(this.__value);
-          }
-
+          __value = __rejecter.apply((this.base || this), this.__value);
         }
         catch(e)
         {
