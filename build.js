5739ebd3faf51c5190bb94dbbf8210295252bb02
keleko34 <Gregory.Guidero@gmail.com>
2019-10-25 01:35:56 +0200

    Build

--- build.js
+++ build.js
@@ -3,11 +3,11 @@ var base = process.cwd().replace(/\\/g,'/'),
     closureCompiler = require('google-closure-compiler-js').compile,
     flags = {};
 
-console.log("Building Library...");
+console.log("Building Peprze Library...");
 
 flags.jsCode = [{src: fs.readFileSync(base+'/peprze.js','utf8')}];
 flags.compilationLevel = 'SIMPLE';
-
+fs.unlinkSync(base+'/peprze.min.js');
 fs.writeFileSync(base+'/peprze.min.js',closureCompiler(flags).compiledCode);
 
 console.log("Finished Building Minified Library..");
